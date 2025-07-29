da1ee46b99cb07658e7b60a5befaed9c9cd3911c
Luan Brandão <luanbrandaodeoliveira@gmail.com>
2025-07-28 23:16:13 -0300

    página principal criada

---
+++ lista.js
@@ -0,0 +1,18 @@
+
+const lista = ["Um", "Dois", "Três"];
+
+export function getLista() {
+    return structuredClone(lista);
+}
+
+export function limpaLista() {
+    lista.splice(0);
+}
+
+export function adicionaNaLista(item) {
+    lista.push(item);
+}
+
+export function removeDaLista(index) {
+    lista.splice(index, 1);
+}
